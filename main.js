70f72a959e992b844c50d7b1160c56de9296d689
alhang <alhang@ucsd.edu>
2023-03-01 18:08:50 -0800

    google tag

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-import {betterAlert, betterConfirm, betterPrompt} from './customdialog.js';
+window.onload = greet;
+
+function greet(){
+    let date = new Date();
+    let hrs = date.getHours();
+    let out;
+
+    if (hrs < 12)
+        out = 'Good Morning';
+    else if (hrs >= 12 && hrs <= 18)
+        out = 'Good Afternoon';
+    else if (hrs >= 18 && hrs <= 24)
+        out = 'Good Evening';
+
+    document.getElementById('greeting').innerHTML = '<i>' + out + '</i> and welcome to my digital portfolio!';
+}
+
